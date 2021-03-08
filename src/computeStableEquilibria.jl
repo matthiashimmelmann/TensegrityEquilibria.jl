13046140fc6278d2470b3f8c49b527c5350b923d
Matthias Himmelmann <65544132+matthiashimmelmann@users.noreply.github.com>
2021-03-08 10:50:46 +0100

    Added capability for static Plots
    + `knownBars` and `knownCables` inputs added solely for plotting purposes.
    + The plotting library `Makie.jl` is used.
    + The parameters in the vertices are replaced with the value in the corresponding configuration that is calculated in the method `stableEquilibria`

--- src/computeStableEquilibria.jl
+++ src/computeStableEquilibria.jl
@@ -1,11 +1,11 @@
-using LinearAlgebra, HomotopyContinuation
+using LinearAlgebra, HomotopyContinuation, Makie
 
 #=
 @input vertices::[[p_11,p_12,...], ..., [p_m1,p_m2,...]], unknownBars::[[i,j,l_ij],...], unknowncables::[[i,j,r_ij,e_ij]] with i<j
 Calculate configurations in equilibrium for the given tensegrity framework
 =#
-function stableEquilibria(vertices::Array, unknownBars::Array, unknownCables::Array, listOfInternalVariables::Array, listOfControlParams::Array, targetParams::Array)
-    assertCorrectInput(vertices, unknownBars, unknownCables, listOfInternalVariables, listOfControlParams, targetParams)
+function stableEquilibria(vertices::Array, unknownBars::Array, unknownCables::Array, listOfInternalVariables::Array{Variable,1}, listOfControlParams::Array, targetParams::Array, knownBars::Array, knownCables::Array)
+    assertCorrectInput(vertices, unknownBars, unknownCables, listOfInternalVariables, listOfControlParams, targetParams, knownBars, knownCables)
 
     @var delta[1:length(unknownCables)];
     B=[]; C=[]; G=[]
@@ -34,20 +34,26 @@ function stableEquilibria(vertices::Array, unknownBars::Array, unknownCables::Ar
             S₀ = solutions(res₀)
             H = ParameterHomotopy(F, params₀, params₀)
             solver, _ = solver_startsolutions(H, S₀)
-            res₀ = solve(F; target_parameters = targetParams)
-            reals = filter(en->isempty(filter(x->x<0, en[length(listOfInternalVariables)+1:length(listOfInternalVariables)+length(delta)])) &&
+            target_parameters!(solver, targetParams)
+            res₀ = solve(solver, S₀, threading = false)
+            realSol = filter(en->isempty(filter(x->x<0, en[length(listOfInternalVariables)+1:length(listOfInternalVariables)+length(delta)])) &&
                 isLocalMinimum(listOfInternalVariables, listOfControlParams, delta, lambda, L, G)(real.(en), targetParams), real_solutions(res₀))
         else
             F = System(∇L; variables = [listOfInternalVariables; delta; lambda])
             res₀ = solve(F)
-            reals = filter(en->isempty(filter(x->x<0, en[length(listOfInternalVariables)+1:length(listOfInternalVariables)+length(delta)])) &&
+            realSol = filter(en->isempty(filter(x->x<0, en[length(listOfInternalVariables)+1:length(listOfInternalVariables)+length(delta)])) &&
                 isLocalMinimum(listOfInternalVariables, [], delta, lambda, L, G)(real.(en), []),real_solutions(res₀))
         end
     else
         throw(error("Internal variables need to be provided!"))
     end
-    reals = map(t->t[1:length(listOfInternalVariables)], reals)
-    return(reals, listOfInternalVariables)
+    realSol = map(t->t[1:length(listOfInternalVariables)], realSol)
+
+    vertices = clearArrayOfVariables(vertices, listOfInternalVariables, realSol, listOfControlParams, targetParams)
+    bars = vcat(unknownBars, knownBars)
+    cables = vcat(unknownCables, knownCables)
+    plotWithMakie(vertices, bars, cables)
+    return(realSol, listOfInternalVariables)
 end
 
 # Check if the current configuration is a local minimum of Q
@@ -74,22 +80,79 @@ function isLocalMinimum(listOfInternalVariables, listOfControlParams, delta, lam
 end
 
 # assert that the inputs of stableEquilibria are correct
-function assertCorrectInput(vertices, unknownBars, unknownCables, listOfInternalVariables, listOfControlParams, targetParams)
+function assertCorrectInput(vertices, unknownBars, unknownCables, listOfInternalVariables, listOfControlParams, targetParams, knownBars, knownCables)
     @assert(length(vertices)>1)
     D = length(vertices[1])
     foreach(vx->@assert(length(vx)==D), vertices)
     foreach(bar->@assert(length(bar)==3), unknownBars)
     foreach(cable->@assert(length(cable)==4), unknownCables)
+    foreach(bar->@assert(length(bar)==2), knownBars)
+    foreach(cable->@assert(length(cable)==2), knownCables)
     foreach(var->@assert(typeof(var)==Variable), listOfInternalVariables)
     foreach(par->@assert(typeof(par)==Variable), listOfControlParams)
     @assert(length(listOfControlParams)==length(targetParams))
 end
 
-@var p[1:2]
-display(stableEquilibria([[1,2],[3,4],p],[[1,3,sqrt(2)]],[[2,3,1,1]],p,[],[]))
+# This function extracts the position of the parameters in the vertices and replaces them
+# with their respective computed value. WARNING: This cannot deal with Polynomial-type entries
+# of the vertices yet. Only pure variables are allowed.
+function clearArrayOfVariables(array, listOfInternalVariables, realSol, listOfControlParams, targetParams)
+    subsArray=[]
+    for i in 1:length(array)
+        append!(subsArray,[[]])
+        for j in 1:length(array[i])
+            if(typeof(array[i][j])!=Float64 && typeof(array[i][j])!=Int64)
+                index=findfirst(en->en==array[i][j], listOfInternalVariables)
+                if(index==nothing || typeof(index) == Nothing || typeof(index) == Void)
+                    index=findfirst(en->en==array[i][j], listOfControlParams)
+                    if(index==nothing || typeof(index) == Nothing || typeof(index) == Void)
+                        throw(error("The variable in the array is neither a control nor an internal parameter."))
+                    end
+                    append!(subsArray[i],[targetParams[index]])
+                else
+                    append!(subsArray[i],[realSol[1][index]])
+                end
+            else
+                append!(subsArray[i],[array[i][j]])
+            end
+        end
+    end
+    return(subsArray)
+end
 
-@var p[1:6]
-display(stableEquilibria([p[1:3],p[4:6],[0,1,0], [sin(2*pi/3),cos(2*pi/3),0], [sin(4*pi/3),cos(4*pi/3),0]],[[1,2,1]],[[1,3,1,1],[1,4,1,1],[1,5,1,1],[2,3,1,1],[2,4,1,1],[2,5,1,1]],
-    p,[],[]))
+# Creates a static, non-parametric plot of the vertices (grey) with corresponding bars (black) and cables (blue).
+function plotWithMakie(vertices::Array, bars::Array, cables::Array)
+    scene = Scene()
+    D = length(vertices[1])
+    if(D==2)
+        x = [vx[1] for vx in vertices]; y = [vx[2] for vx in vertices];
+        scene=Makie.scatter(x,y,markersize=25,color=:grey)
+        for line in bars
+            Makie.lines!([vertices[Int64(line[1])][1],vertices[Int64(line[2])][1]], [vertices[Int64(line[1])][2],vertices[Int64(line[2])][2]], linewidth=5,color=:black)
+        end
+        for line in cables
+            Makie.lines!([vertices[Int64(line[1])][1],vertices[Int64(line[2])][1]], [vertices[Int64(line[1])][2],vertices[Int64(line[2])][2]], color=:blue)
+        end
+    elseif(D==3)
+        x = [vx[1] for vx in vertices]; y = [vx[2] for vx in vertices]; z = [vx[3] for vx in vertices]
+        scene=Makie.scatter(x,y,z,markersize=25,color=:grey)
+        for line in bars
+            Makie.lines!([vertices[Int64(line[1])][1],vertices[Int64(line[2])][1]], [vertices[Int64(line[1])][2],vertices[Int64(line[2])][2]], [vertices[Int64(line[1])][3],vertices[Int64(line[2])][3]], linewidth=7, color=:black)
+        end
+        for line in cables
+            Makie.lines!([vertices[Int64(line[1])][1],vertices[Int64(line[2])][1]], [vertices[Int64(line[1])][2],vertices[Int64(line[2])][2]], [vertices[Int64(line[1])][3],vertices[Int64(line[2])][3]], color=:blue)
+        end
+    else
+        throw(error("Plot only supported for 2D and 3D Frameworks."))
+    end
+    display(scene)
+end
+
+@var p[1:2]
+display(stableEquilibria([[1,2],[3,4],p],[[1,3,sqrt(2)]],[[2,3,1,1]],p,[],[],[],[]))
+
+@var p[1:6] ell
+display(stableEquilibria([p[1:3],p[4:6],[0,1,0], [sin(2*pi/3),cos(2*pi/3),0], [sin(4*pi/3),cos(4*pi/3),0]],[[1,2,ell]],[[1,3,1,1],[1,4,1,1],[1,5,1,1],[2,3,1,1],[2,4,1,1],[2,5,1,1]],
+    p,[ell],[1],[[3,4],[3,5],[4,5]],[]))
 
 #TODO Makie plot: listOfInternalVariables->solution
